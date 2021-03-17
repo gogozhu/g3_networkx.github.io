40da5e3c1ae16890c601ef3e5bb930aef5d2ff9b
gogozhu <1003457625@qq.com>
2021-03-17 16:48:58 +0800

    Update graphin.js

--- graphin.js
+++ graphin.js
@@ -61,71 +61,20 @@ function createStyleElement() {
 // 若拷贝官方代码，别忘了 npm install insert-css
 insertCss(`
   .g6-component-tooltip {
-    background-color: rgba(255, 255, 255, 0.8);
-    padding: 0px 10px 24px 10px;
+    border: 1px solid #e2e2e2;
+    border-radius: 4px;
+    font-size: 12px;
+    color: #000;
+    background-color: rgba(255, 255, 255, 0.9);
+    padding: 10px 8px;
     box-shadow: rgb(174, 174, 174) 0px 0px 10px;
   }
 `);
 
-const data = {
-  nodes: [
-    {
-      id: '0',
-      label: 'node-0',
-      x: 100,
-      y: 100,
-      description: 'This is node-0.',
-      subdescription: 'This is subdescription of node-0.',
-    },
-    {
-      id: '1',
-      label: 'node-1',
-      x: 250,
-      y: 100,
-      description: 'This is node-1.',
-      subdescription: 'This is subdescription of node-1.',
-    },
-    {
-      id: '2',
-      label: 'node-2',
-      x: 150,
-      y: 310,
-      description: 'This is node-2.',
-      subdescription: 'This is subdescription of node-2.',
-    },
-    {
-      id: '3',
-      label: 'node-3',
-      x: 320,
-      y: 310,
-      description: 'This is node-3.',
-      subdescription: 'This is subdescription of node-3.',
-    },
-  ],
-  edges: [
-    {
-      id: 'e0',
-      source: '0',
-      target: '1',
-      description: 'This is edge from node 0 to node 1.',
-    },
-    {
-      id: 'e1',
-      source: '0',
-      target: '2',
-      description: 'This is edge from node 0 to node 2.',
-    },
-    {
-      id: 'e2',
-      source: '0',
-      target: '3',
-      description: 'This is edge from node 0 to node 3.',
-    },
-  ],
-};
 const tooltip = new G6.Tooltip({
   offsetX: 10,
   offsetY: 10,
+  fixToNode: [1, 0.5],
   // the types of items that allow the tooltip show up
   // 允许出现 tooltip 的 item 类型
   itemTypes: ['node', 'edge'],
@@ -134,15 +83,15 @@ const tooltip = new G6.Tooltip({
   getContent: (e) => {
     const outDiv = document.createElement('div');
     outDiv.style.width = 'fit-content';
-    //outDiv.style.padding = '0px 0px 20px 0px';
-    outDiv.innerHTML = `
-      <h4>Custom Content</h4>
-      <ul>
-        <li>Type: ${e.item.getType()}</li>
-      </ul>
-      <ul>
-        <li>Label: ${e.item.getModel().label || e.item.getModel().id}</li>
-      </ul>`;
+    outDiv.style.height = 'fit-content';
+    const model = e.item.getModel();
+    if (e.item.getType() === 'node') {
+      outDiv.innerHTML = `${model.name}<br/>${model.keyNo}`;
+    } else {
+      const source = e.item.getSource();
+      const target = e.item.getTarget();
+      outDiv.innerHTML = `来源：${source.getModel().name}<br/>去向：${target.getModel().name}`;
+    }
     return outDiv;
   },
 });
@@ -154,37 +103,61 @@ const graph = new G6.Graph({
   container: 'container',
   width,
   height,
-  linkCenter: true,
+  layout: {
+    type: 'force',
+    edgeStrength: 0.7,
+  },
   plugins: [tooltip],
   modes: {
-    default: ['drag-node'],
+    default: ['drag-canvas', 'activate-relations'],
   },
   defaultNode: {
-    size: [80, 40],
-    type: 'rect',
+    size: [10, 10],
+    /* style for the keyShape */
+    // style: {
+    //   lineWidth: 2,
+    //   fill: '#DEE9FF',
+    //   stroke: '#5B8FF9',
+    // },
   },
   defaultEdge: {
+    /* style for the keyShape */
     style: {
-      stroke: '#b5b5b5',
-      lineAppendWidth: 3,
+      stroke: '#aaa',
+      lineAppendWidth: 2,
+      opacity: 0.3,
     },
   },
+  /* styles for different states, there are built-in styles for states: active, inactive, selected, highlight, disable */
+  // nodeStateStyles: {
+  //   active: {
+  //     opacity: 1,
+  //   },
+  //   inactive: {
+  //     opacity: 0.2,
+  //   },
+  // },
+  // edgeStateStyles: {
+  //   active: {
+  //     stroke: '#999',
+  //   },
+  // },
 });
-graph.data(data);
-graph.render();
 
-graph.on('node:mouseenter', (e) => {
-  graph.setItemState(e.item, 'active', true);
-});
-graph.on('node:mouseleave', (e) => {
-  graph.setItemState(e.item, 'active', false);
-});
-graph.on('edge:mouseenter', (e) => {
-  graph.setItemState(e.item, 'active', true);
-});
-graph.on('edge:mouseleave', (e) => {
-  graph.setItemState(e.item, 'active', false);
-});
+fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/xiaomi.json')
+  .then((res) => res.json())
+  .then((data) => {
+    graph.data(data);
+    graph.render();
+  });
+
+if (typeof window !== 'undefined')
+  window.onresize = () => {
+    if (!graph || graph.get('destroyed')) return;
+    if (!container || !container.scrollWidth || !container.scrollHeight) return;
+    graph.changeSize(container.scrollWidth, container.scrollHeight);
+  };
+
 
 if (typeof window !== 'undefined')
   window.onresize = () => {
